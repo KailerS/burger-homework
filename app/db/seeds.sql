bfe4269b7977830ecd1ccba62c1e27d7665f0b70
KailerS <KailerS@users.noreply.github.com>
2019-10-17 18:01:05 -0600

    added files and started on orm and controllers.

---
+++ app/db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burger (name) VALUES ('BBQ Burger');
+INSERT INTO burger (name) VALUES ('Hamburger');
---
+++ app/models/burger.js
@@ -0,0 +1,20 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+  all: function(cb) {
+    orm.all("burger", function(res) {
+      cb(res);
+    });
+  },
+  create: function(cols, vals, cb) {
+    orm.create("burger", cols, vals, function(res) {
+      cb(res);
+    });
+  },
+  update: function(objColVals, condition, cb) {
+    orm.update("burger", objColVals, condition, function(res) {
+      cb(res);
+    });
+  }
+};
+module.exports = burger;
